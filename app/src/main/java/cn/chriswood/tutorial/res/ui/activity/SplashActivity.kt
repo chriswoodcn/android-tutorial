09594aec8d09c1c50510b4149936feb4e5a2e717
chriswoodcn <chriswoodcn@aliyun.com>
2024-07-19 13:20:26 +0800

    [update] 启动页样式修改，Handler方法过期修复

--- app/src/main/java/cn/chriswood/tutorial/res/ui/activity/SplashActivity.kt
+++ app/src/main/java/cn/chriswood/tutorial/res/ui/activity/SplashActivity.kt
@@ -1,16 +1,19 @@
 package cn.chriswood.tutorial.res.ui.activity
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import cn.chriswood.tutorial.MainActivity
 import cn.chriswood.tutorial.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : Activity() {
-    private val mHandler = Handler()
+    private val mHandler = Handler(Looper.getMainLooper())
     private lateinit var mBtnSkip: Button
     private val mRunnableToMain = Runnable { toMainActivity() }
     private fun toMainActivity() {
@@ -23,7 +26,7 @@ class SplashActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        mBtnSkip = findViewById(R.id.id_btn_skip) as Button
+        mBtnSkip = findViewById<Button>(R.id.id_btn_skip)
         mHandler.postDelayed(mRunnableToMain, 3000)
 
         mBtnSkip.setOnClickListener(View.OnClickListener {
