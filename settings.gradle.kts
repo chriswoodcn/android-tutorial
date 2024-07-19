2bfbc0416135ee9361e886e30fefd36fcdaead42
chriswoodcn <chriswoodcn@aliyun.com>
2024-07-19 21:16:28 +0800

    [update] 使用gradle wrapper 设置 repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,47 +1,39 @@
-//init.gradle.kts
-//fun RepositoryHandler.setPluginRepos(){
-//    // maven("http://localhost:8001/repository/maven-plugins/"){
-//    //     isAllowInsecureProtocol=true
-//    // }
-//    maven("https://maven.aliyun.com/repository/google")
-//    maven("https://maven.aliyun.com/repository/gradle-plugin")
-//    gradlePluginPortal()
-//    google{mavenContent{
-//        includeGroupAndSubgroups("androidx")
-//        includeGroupAndSubgroups("com.android")
-//        includeGroupAndSubgroups("com.google")
-//    }}
-//}
-//fun RepositoryHandler.setDependenceRepos(){
-//    // maven("http://localhost:8001/repository/maven-public/"){
-//    //     isAllowInsecureProtocol=true
-//    // }
-//    maven("https://maven.aliyun.com/repository/public")
-//    maven("https://maven.aliyun.com/repository/central")
-//    maven("https://maven.aliyun.com/repository/jcenter")
-//    maven("https://maven.aliyun.com/repository/google")
-//    maven("https://maven.aliyun.com/repository/gradle-plugin")
-//    mavenCentral()
-//    mavenLocal()
-//    google{mavenContent{
-//        includeGroupAndSubgroups("androidx")
-//        includeGroupAndSubgroups("com.android")
-//        includeGroupAndSubgroups("com.google")
-//    }}
-//}
-//settingsEvaluated {
-//    pluginManagement {
-//        repositories {
-//            setPluginRepos()
-//        }
-//    }
-//    dependencyResolutionManagement {
-//        repositories{
-//            setDependenceRepos()
-//        }
-//    }
-//}
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/jcenter")
+        maven("https://maven.aliyun.com/repository/google") {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
+dependencyResolutionManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/jcenter")
+        maven("https://maven.aliyun.com/repository/google") {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        google()
+        mavenCentral()
+    }
+}
 
 rootProject.name = "android-tutorial"
 include(":app")
